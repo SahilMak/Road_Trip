934a9f7bae4f1137a3110cb4bb9437e801635a37
Sahil Makhijani <svmakhijani94@gmail.com>
2016-04-28 15:01:46 -0400

    Added largest mobile element mover

--- road_trip.py
+++ road_trip.py
@@ -7,42 +7,33 @@ def main():
   # Initialize shortest path variables
   shortest = math.inf
   path = []
-  
   # Get file and starting city from command line
   file = open(sys.argv[1])
   start = sys.argv[2]
-  
   # Store city names in list
   city = file.readline().split(" ")
   length = len(city)
   city[length - 1] = city[length - 1].rstrip('\n')
-  
   # Store 2 ordered cities in list
   order = file.readline.split(" ")
   if len(order) == 1:
     order = [start, start]
   else:
     order[1] = order[1].rstrip('\n')
-  
   # Store distances in list
   distance = [line.split() for line in file]
-  
   # Close file
   file.close()
-  
   # Create list of directions
   direction = [False] * (length - 1)
-  
   # Create dictionary from list of cities
   num = {city[0]:0}
   for x in range(1, length):
     num[city[x]] = x
-  
   # Add starting city to beginning and end of list
   city.remove(start)
   city.insert(0, start)
   city.append(start)
-  
   # Find length of initial path (if valid)
   if city.index(order[0]) <= city.index(order[1]):
     for x in range(length):
@@ -51,7 +42,6 @@ def main():
       else:
         shortest += int(distance[num[city[x]]][num[city[x + 1]]])
     path = city[:]
-  
   # Find shortest path
   short(length)
 
@@ -70,6 +60,12 @@ def short(length):
         largest = num[city[x]]
         index = x
         swap = direction[x - 1]
-    #
+    # Swap elements
+    if not(swap):
+      city[index], city[index - 1] = city[index - 1], city[index]
+      direction[index - 1], direction[index - 2] = direction[index - 2], direction[index - 1]
+    else:
+      city[index], city[index + 1] = city[index + 1], city[index]
+      direction[index - 1], direction[index] = direction[index], direction[index - 1]
 
 main()
